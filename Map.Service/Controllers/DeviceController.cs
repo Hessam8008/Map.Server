72973a41135289269a915187bb34eb810e308108
Hessam Hosseini <Hessam8008@yahoo.com>
2020-07-19 09:22:06 +0430

    Update: Add methods to DeviceController

--- Map.Service/Controllers/DeviceController.cs
+++ Map.Service/Controllers/DeviceController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Map.Models;
+using Map.Models.AVL;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,8 +13,34 @@ namespace Map.Service.Controllers
     [ApiController]
     public class DeviceController : ControllerBase
     {
+        private readonly IMapUnitOfWork unitOfWork;
 
+
+        public DeviceController(IMapUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Device>> GetAllAsync()
+        {
+            var result = await unitOfWork.DeviceRepository.GetAllAsync();
+            return result;
+        }
         
+        [HttpGet]
+        public async Task<Device> GetByIMEIAsync(string imei)
+        {
+            var result = await unitOfWork.DeviceRepository.GetByIMEIAsync(imei);
+            return result;
+        }
+        
+        [HttpGet]
+        public async Task<Device> GetByIdAsync(int id)
+        {
+            var result = await unitOfWork.DeviceRepository.GetByIdAsync(id);
+            return result;
+        }
 
     }
 }
