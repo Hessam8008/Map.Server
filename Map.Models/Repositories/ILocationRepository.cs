817172ec7922fee4e427fc968257b1e74599cd72
Hessam Hosseini <Hessam8008@yahoo.com>
2020-07-19 08:28:02 +0430

    Add ILocationRepository

---
+++ Map.Models/Repositories/ILocationRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Map.Models.AVL;
+
+namespace Map.Models.Repositories
+{
+    public interface ILocationRepository
+    {
+        public Task<int> InsertAsync(Location location);
+
+        public Task<IEnumerable<Location>> GetAsync(int deviceId, DateTime from, DateTime to);
+
+    }
+}
