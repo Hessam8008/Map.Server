2e88f695beb2ebd4348d5bb939a0943a551a42d9
Hessam Hosseini <Hessam8008@yahoo.com>
2020-07-19 10:06:53 +0430

    Add Swagger

--- Map.Service/Startup.cs
+++ Map.Service/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Map.DataAccess;
 using Map.Models;
@@ -11,6 +13,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace Map.Service
 {
@@ -26,7 +30,11 @@ namespace Map.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            AddSwagger(services);
 
             services.AddScoped<IDatabaseSettings, DatabaseSettings>();
             services.AddScoped<IMapUnitOfWork, MapUnitOfWork>();
@@ -41,6 +49,8 @@ namespace Map.Service
                 app.UseDeveloperExceptionPage();
             }
 
+            ConfigSwagger(app);
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -48,6 +58,37 @@ namespace Map.Service
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+        }
+
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Map API", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        private void ConfigSwagger(IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Map API V1");
+                c.RoutePrefix = string.Empty;
+                c.DocExpansion(DocExpansion.None);
+                c.DefaultModelsExpandDepth(-1);
+                c.DisplayRequestDuration();
+                c.EnableDeepLinking();
+                c.DefaultModelRendering(ModelRendering.Example);
+                c.EnableFilter();
+                c.ShowExtensions();
+                c.EnableValidator();
             });
         }
     }
