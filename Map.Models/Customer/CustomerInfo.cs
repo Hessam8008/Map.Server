14162c0f497a3e55bf02fc04dbe37c6f4ea9ed4d
Hessam Hoseini <hessam8008@hotmail.com>
2020-09-01 13:07:16 +0430

    Add Customer and related interfaces

---
+++ Map.Models/Customer/CustomerInfo.cs
@@ -0,0 +1,54 @@
+﻿namespace Map.Models.Customer
+{
+    /// <summary>
+    /// The customer information.
+    /// </summary>
+    public class CustomerInfo
+    {
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address.
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Latitude.
+        /// </summary>
+        /// <value>The latitude.</value>
+        public double Latitude { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Longitude.
+        /// </summary>
+        /// <value>The longitude.</value>
+        public double Longitude { get; set; }
+        
+        /// <summary>
+        /// Gets or sets a value indicating whether active.
+        /// </summary>
+        public bool Active { get; set; }
+
+        /// <summary>
+        /// Gets or sets the level.
+        /// </summary>
+        public byte Grade
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the area (Branch).
+        /// </summary>
+        public int Area { get; set; }
+    }
+}
---
+++ Map.Service/Controllers/CustomerController.cs
@@ -0,0 +1,53 @@
+﻿namespace Map.Service.Controllers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Map.Models;
+    using Map.Models.AVL;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// The customer controller.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        /// <summary>
+        /// The unit of work
+        /// </summary>
+        private readonly IMapUnitOfWork unitOfWork;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomerController"/> class.
+        /// </summary>
+        /// <param name="unitOfWork">The unit of work.</param>
+        public CustomerController(IMapUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        /// <summary>
+        /// Get all devices registered in the system.
+        /// </summary>
+        /// <returns>List of devices</returns>
+        /// <response code="200">Returns a list of devices.</response>
+        /// <response code="204">If no device found.</response>
+        [ProducesResponseType(typeof(IEnumerable<Device>), 200)]
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var result = await this.unitOfWork.DeviceRepository.GetAllAsync();
+            if (result == null || !result.Any())
+            {
+                return this.NoContent();
+            }
+
+            return this.Ok(result);
+        }
+
+    }
+}
