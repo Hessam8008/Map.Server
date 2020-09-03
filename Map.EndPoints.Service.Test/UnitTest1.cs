883fd6fc7e7d8e6fb8e8996f1ef06459c7223394
Hessam Hoseini <hessam8008@hotmail.com>
2020-09-03 13:21:56 +0430

    Add project: Map.EndPoints.Service.Test

---
+++ Map.EndPoints.Service.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Map.EndPoints.Service.Test
+{
+    [TestClass]
+    public class LocationServiceTest
+    {
+        [TestMethod]
+        public void GetPath()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
