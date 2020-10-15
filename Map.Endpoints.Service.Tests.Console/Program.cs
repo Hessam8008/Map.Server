a0cb6d3e801c509a144eaa37c8415035ce58c662
U12745 <U12745@DM1PC3>
2020-10-15 11:48:20 +0330

    Add Test.Console Project

---
+++ Map.Endpoints.Service.Tests.Console/Program.cs
@@ -0,0 +1,50 @@
+﻿using Map.EndPoints.Service;
+using Map.EndPoints.Service.Models;
+using Services.WebApiCaller;
+using Services.WebApiCaller.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace Map.Endpoints.Service.Tests.Console
+{
+    public class Program
+    {
+        private static async Task Main(string[] _)
+        {
+
+            var _caller = new ApiCaller();
+            var _config = new WinApiConfiguration();
+
+            _config.Load();
+
+            var ms = new MapService(_caller, _config);
+
+            //await ms.DeviceService.DeleteAsync(70);
+
+            var result = await ms.DeviceService.CreateAsync(new Device()
+            {
+                IMEI = "123456789",
+                Model = "1",
+                CreateTime = DateTime.Now,
+                OwnerMobileNumber = "91555",
+                SN = "134",
+                Nickname = "sss",
+                SimNumber = "121"
+            });
+
+            await ms.DeviceService.UpdateAsync(new Device()
+            {
+                ID = 71,
+                IMEI = "123",
+                Model = "1",
+                CreateTime = DateTime.Now,
+                OwnerMobileNumber = "91555",
+                SN = "134",
+                Nickname = "sss",
+                SimNumber = "121"
+            });
+
+            //System.Console.WriteLine(result?.ToString());
+        }
+    }
+}
