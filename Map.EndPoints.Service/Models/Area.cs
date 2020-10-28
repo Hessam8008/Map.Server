7d40f9e373beec6cdcc450c8e505a64681b2e5fd
U12745 <U12745@DM1PC3>
2020-10-28 14:47:38 +0330

    Add Area Model

---
+++ Map.EndPoints.Service/Models/Area.cs
@@ -0,0 +1,13 @@
+﻿namespace Map.EndPoints.Service.Models
+{
+    public class Area
+    {
+        /// <summary>Gets or sets the identifier.</summary>
+        /// <value>The identifier.</value>
+        public int ID { get; set; }
+
+        /// <summary>Gets or sets the title.</summary>
+        /// <value>The title.</value>
+        public string Title { get; set; }
+    }
+}
