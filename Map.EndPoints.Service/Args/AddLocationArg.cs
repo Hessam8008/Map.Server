5c4fcff746aa9d180bf28e634863aec08e3a18a6
U12745 <U12745@DM1PC3>
2020-11-01 12:07:04 +0330

    Update MapService

---
+++ Map.EndPoints.Service/Args/AddLocationArg.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Map.EndPoints.Service.Models;
+
+namespace Map.EndPoints.Service.Args
+{
+    public class AddLocationArg
+    {
+        public int DeviceID { get; set; }
+        /// <summary>
+        /// Gets or sets the Time.
+        /// </summary>
+        /// <value>The time.</value>
+        public DateTime Time { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Longitude.
+        /// </summary>
+        /// <value>The longitude.</value>
+        public double Longitude { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Latitude.
+        /// </summary>
+        /// <value>The latitude.</value>
+        public double Latitude { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Altitude.
+        /// </summary>
+        /// <value>The altitude.</value>
+        public short Altitude { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Angle.
+        /// </summary>
+        /// <value>The angle.</value>
+        public short Angle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Satellites.
+        /// </summary>
+        /// <value>The satellites.</value>
+        public byte Satellites { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Speed.
+        /// </summary>
+        /// <value>The speed.</value>
+        public short Speed { get; set; }
+
+        /// <summary>
+        /// Gets or sets the IoElements.
+        /// </summary>
+        /// <value>The elements.</value>
+        public List<LocationElement> Elements { get; set; }
+    }
+}
--- Map.EndPoints.Service/SubServices/LocationService.cs
+++ Map.EndPoints.Service/SubServices/LocationService.cs
@@ -1,15 +1,17 @@
-﻿namespace Map.EndPoints.Service.SubServices
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Globalization;
-    using System.Threading.Tasks;
+﻿using Map.EndPoints.Service.Args;
+using Services.Core.Tools;
+using System.Net.Http;
+using System.Text;
 
+namespace Map.EndPoints.Service.SubServices
+{
     using Map.EndPoints.Service.Interfaces;
     using Map.EndPoints.Service.Models;
-
     using Services.Core;
     using Services.Core.Interfaces;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
 
     internal class LocationService : AbstractSubService, ILocationService
     {
@@ -18,17 +20,41 @@
         {
         }
 
+        //public async Task CreateAsync(Location location)
+        //{
+        //    var body = location.ToJson();
+        //    var param = new RequestParameters
+        //    {
+        //        Path = $"{RootUrl}/Create",
+        //        Content = new StringContent(body, Encoding.UTF8, MediaTypeNames.ApplicationJson)
+        //    };
+        //    var response = await ApiService.PostAsync(param).ConfigureAwait(false);
+        //    await ParseAsync(response).ConfigureAwait(false);
+        //}
+
+        public async Task CreateAsync(AddLocationArg location)
+        {
+            var body = location.ToJson();
+            var param = new RequestParameters
+            {
+                Path = $"{RootUrl}/Create",
+                Content = new StringContent(body, Encoding.UTF8, MediaTypeNames.ApplicationJson)
+            };
+            var response = await ApiService.PostAsync(param).ConfigureAwait(false);
+            await ParseAsync(response).ConfigureAwait(false);
+        }
+
         public async Task<List<Location>> GetLocationsAsync(int deviceId, DateTime @from, DateTime to)
         {
-            var param = new RequestParameters { Path = $"{this.RootUrl}/GetLocations" };
+            var param = new RequestParameters { Path = $"{RootUrl}/GetLocations" };
             param.AddUrlParameter(nameof(deviceId), deviceId.ToString());
             param.AddUrlParameter(nameof(from), from.ToString("yyyy-MM-dd HH:mm"));
             param.AddUrlParameter(nameof(to), to.ToString("yyyy-MM-dd HH:mm"));
-            
+
             Console.WriteLine("httpTarget: {0}", param.GetUri());
 
-            var response = await this.ApiService.GetAsync(param).ConfigureAwait(false);
-            return await this.ParseAsync<List<Location>>(response).ConfigureAwait(false);
+            var response = await ApiService.GetAsync(param).ConfigureAwait(false);
+            return await ParseAsync<List<Location>>(response).ConfigureAwait(false);
         }
     }
 }
